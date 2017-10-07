95d39f59c6a2f5b3adee00c1ac5a416a6d9a078b
Arnav Gupta <arnav@codingblocks.com>
2017-10-08 00:04:40 +0530

    colorssss
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- apps/rainbow/index.js
+++ apps/rainbow/index.js
@@ -1,4 +1,10 @@
 const hooloovoo = require('hooloovoo');
-hooloovoo.setup(6);
+hooloovoo.setup(7);
 
-hooloovoo.fill_hex("FF0000");
+hooloovoo.set_pixel_RGB(0,255,0,0);
+hooloovoo.set_pixel_RGB(1, 0, 255, 0);
+hooloovoo.set_pixel_RGB(2, 0, 0, 255);
+hooloovoo.set_pixel_RGB(3, 255, 255, 0);
+hooloovoo.set_pixel_RGB(4, 0, 255, 255);
+hooloovoo.set_pixel_RGB(5, 255, 0, 255);
+hooloovoo.set_pixel_RGB(6, 255,255,255);
