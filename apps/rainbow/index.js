9aef5893551c9559221d79b45e6476b0f1ac8acc
Arnav Gupta <arnav@codingblocks.com>
2017-10-07 23:48:47 +0530

    yay rainbows
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ apps/rainbow/index.js
@@ -0,0 +1,10 @@
+var Apa102spi = require('apa102-spi')
+
+// Apa102spi(number of leds, clock divider)
+var LedDriver = new Apa102spi(6, 100)
+
+// setLedColor(n, brightness 0-31, red 0-255, green 0-255, blue 0-255)
+LedDriver.setLedColor(0, 20, 255, 0, 0)
+
+// send data to led string
+LedDriver.sendLeds()
